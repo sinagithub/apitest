0026ed09cda2bb0772759ceb20c42ea92ed19b82
yahyakara <yahyak.kara@gmail.com>
2021-02-26 16:15:05 +0300

    Vendor listing scenario template added

--- src/test/java/stepDefinitions/AccountSteps.java
+++ src/test/java/stepDefinitions/AccountSteps.java
@@ -4,10 +4,7 @@ package stepDefinitions;
 import apiEngine.Utils;
 import apiEngine.models.requests.AuthorizationRequest;
 import cucumber.TestContext;
-import enums.Context;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.restassured.http.ContentType;
 
 import java.io.IOException;
 
@@ -17,15 +14,19 @@ public class AccountSteps extends BaseSteps {
         super(testContext);
     }
 
-    @Given("I am an authorized  with {string} user")
-    public void i_am_an_authorized_with_user(String userType) throws IOException {
+    @Given("I am an authorized  user {string}")
+    public void i_am_an_authorized_user(String userType) throws IOException {
         String userName = Utils.getGlobalValue("test1_userName");
         String passWord = Utils.getGlobalValue("test1_password");
         AuthorizationRequest authRequest = new AuthorizationRequest(userName, passWord);
         getOauthCoreClient().authenticateUser(authRequest, userType.equalsIgnoreCase("Login"));
+    }
 
+    @Given("I have pinned address")
+    public void i_have_pinned_address() {
 
     }
+
 
 
 }
