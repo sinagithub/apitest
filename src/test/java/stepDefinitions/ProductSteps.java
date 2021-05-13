f42b5dd16a1abb603b8b7777f3f4b26b1e6ccf6c
yahyakara <yahyak.kara@gmail.com>
2021-05-14 01:35:53 +0300

    Product listing favorite staus secenario added

--- src/test/java/stepDefinitions/ProductSteps.java
+++ src/test/java/stepDefinitions/ProductSteps.java
@@ -1,7 +1,10 @@
 package stepDefinitions;
 
 
+import apiEngine.IRestResponse;
 import apiEngine.models.response.ProductDetail.Data;
+import apiEngine.models.response.ProductDetail.Product;
+import apiEngine.models.response.ProductDetail.ProductResponse;
 import cucumber.TestContext;
 import enums.Context;
 import io.cucumber.java.en.Then;
@@ -15,23 +18,22 @@ public class ProductSteps extends BaseSteps {
     }
 
 
-
     @Then("I validate product name is valid on product detail")
     public void validateProductNameIsValid() {
         Data productData = (Data) getScenarioContext().getContext(Context.PRODUCT_DETAIL_DATA);
-        assertNotNull(productData.getName(),"Product name must not null");
+        assertNotNull(productData.getName(), "Product name must not null");
     }
 
     @Then("I validate product desc is valid on product detail")
     public void validateDescIsValid() {
         Data productData = (Data) getScenarioContext().getContext(Context.PRODUCT_DETAIL_DATA);
-        assertNotNull(productData.getDescription(),"Product desc must not null");
+        assertNotNull(productData.getDescription(), "Product desc must not null");
     }
 
     @Then("I validate product Badge is valid on product detail")
     public void validateProductBadgeIsValid() {
         Data productData = (Data) getScenarioContext().getContext(Context.PRODUCT_DETAIL_DATA);
-        assertNotNull(productData.getBadgeType(),"Badge type must not null");
+        assertNotNull(productData.getBadgeType(), "Badge type must not null");
     }
 
     @Then("I validate product unit mass is valid on product detail")
@@ -53,7 +55,7 @@ public class ProductSteps extends BaseSteps {
     @Then("I validate product price is valid on product detail")
     public void validateProductPrice() {
         Data productData = (Data) getScenarioContext().getContext(Context.PRODUCT_DETAIL_DATA);
-        assertNotNull(productData.getPrice(),"Product price must not null");
+        assertNotNull(productData.getPrice(), "Product price must not null");
     }
 
     @Then("I validate product is {string} on product detail")
@@ -61,5 +63,18 @@ public class ProductSteps extends BaseSteps {
         Data productData = (Data) getScenarioContext().getContext(Context.PRODUCT_DETAIL_DATA);
         boolean selectType = isActive.equalsIgnoreCase("true");
         assertTrue(productData.getIsActive() == selectType, "Product should be " + isActive);
+    }
+
+    @Then("I can check product favorite status {string}")
+    public void i_can_check_product_favorite_status(String favoriteStatus) {
+        IRestResponse<ProductResponse> selectedProductResponse =
+                (IRestResponse<ProductResponse>) getScenarioContext().getContext(Context.PRODUCT_DETAIL_RESPONSE);
+
+        if (favoriteStatus.equalsIgnoreCase("True")) {
+            assertTrue(selectedProductResponse.getBody().getData().getFavorite(),"Favorite status should be true");
+        }
+        else {
+            assertTrue(!selectedProductResponse.getBody().getData().getFavorite(),"Favorite status should be false");
+        }
     }
 }
