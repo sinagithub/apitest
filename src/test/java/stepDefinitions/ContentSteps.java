b15e03de69a3f484f326ab45843084f964c24b00
yahyakara <yahyak.kara@gmail.com>
2021-05-20 15:39:39 +0300

    Add Content Faq controls

--- src/test/java/stepDefinitions/ContentSteps.java
+++ src/test/java/stepDefinitions/ContentSteps.java
@@ -1,45 +1,69 @@
 package stepDefinitions;
 
+import apiEngine.IRestResponse;
+import apiEngine.models.response.Content.FaqData;
+import clients.carsi.CarsiContentClient;
 import cucumber.TestContext;
+import enums.Context;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+
 public class ContentSteps extends BaseSteps{
+    
+    private List<FaqData> getFaqDataList(){
+        return (List<FaqData>) getScenarioContext().getContext(Context.GET_CONTENT_LIST);
+    }
+
+    private FaqData getSelectedFaqData(){
+        return (FaqData) getScenarioContext().getContext(Context.SELECTED_CONTENT_DATA);
+    }
+
     public ContentSteps(TestContext testContext) {
         super(testContext);
     }
-
-    @When("I can list operation guide")
-    public void i_can_list_operation_guide() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I can validate operation guide is valid")
-    public void i_can_validate_operation_guide_is_valid() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("I can list Faq content")
+    public void i_can_list_faq_content() {
+        List<FaqData> getFaqContentList = getCarsiContentClient().getFaqResponse().getBody().getData();
+        getScenarioContext().setContext(Context.GET_CONTENT_LIST, getFaqContentList);
     }
 
-    @When("I can list faq list")
-    public void i_can_list_faq_list() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("I can validate faq is valid")
-    public void i_can_validate_faq_is_valid() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("I can list about")
-    public void i_can_list_about() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("I select operation guide with id {string}")
+    public void i_select_operation_guide_with_id(String expectedId) {
+        FaqData selectedFaqData = new FaqData();
+        List<FaqData> getFaqContentList = getFaqDataList();
+        for (FaqData faqData : getFaqContentList){
+            if (faqData.getId().equalsIgnoreCase(expectedId)){
+                selectedFaqData = faqData;
+                break;
+            }
+        }
+        getScenarioContext().setContext(Context.SELECTED_CONTENT_DATA, selectedFaqData);
     }
 
-    @Then("I can validate about is valid")
-    public void i_can_validate_about_is_valid() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Then("I can validate selected operation guide text is {string}")
+    public void i_can_validate_selected_operation_guide_text_is(String expectedText) {
+       String actualSelectedFaqData = getSelectedFaqData().getText();
+       assertEqual("Content text should be valid : " ,actualSelectedFaqData,expectedText);
+    }
+
+    @Then("I can validate selected operation guide header is {string}")
+    public void i_can_validate_operation_guide_title_is(String expectedHeader) {
+        String actualHeader = getSelectedFaqData().getHeader();
+        assertEqual("Content header should be valid : " ,actualHeader,expectedHeader);
+    }
+
+    @Then("I can validate selected operation guide is active {string}")
+    public void i_can_validate_operation_guide_is_active(String contentStatus) {
+        Boolean actualContentStatus = getSelectedFaqData().getIsActive();
+        if (contentStatus.equalsIgnoreCase("True")){
+            assertTrue(actualContentStatus, "Content status should be true");
+
+        }
+        else {
+            assertTrue(!actualContentStatus, "Content status should be false");
+        }
+
     }
 }
--- src/test/resources/features/content.feature
+++ src/test/resources/features/content.feature
@@ -4,17 +4,16 @@ Feature: Splash Api controls
   Background: Login user
     Given I am an authorized  user "Login"
 
-  Scenario: User can list Operation Guide
+  Scenario Outline: User can list Operation Guide
     Given I select city "TR_ISTANBUL"
-    When I can list operation guide
-    Then I can validate operation guide is valid
+    When I can list Faq content
+    Then I select operation guide with id "<GuideId>"
+    Then I can validate selected operation guide text is "<GuideText>"
+    Then I can validate selected operation guide header is "<GuideHeader>"
+    And I can validate selected operation guide is active "<GuideStatus>"
 
-  Scenario: User can list faq list
-    Given I select city "TR_ISTANBUL"
-    When I can list faq list
-    Then I can validate faq is valid
+    Examples: Operation guide data content
+      | GuideId                              | GuideHeader                                   | GuideStatus | GuideText                                                                                                                                                                                                                                                     |
+        | 1dfb20c3-eea0-432e-bed7-ead6cf81cb72 | Nasıl sipariş verebilirim?                    | true        | Bulunduğunuz semti seçtikten sonra listelenen restoranlar arasından damak tadınıza uygun olanı bulabilir ve dilediğiniz yemekleri sepete atabilirsiniz. Adresinizi ve ödeme şeklinizi seçtikten sonra siparişinizi tamamlayabilirsiniz. Hepsi bu kadar basit. |
+      | c693caf6-d0bb-4606-9fec-ee0061377c7b | Bilgilerimi restoranlarla paylaşacak mısınız? | false       | Siparişinizin size sorunsuz ulaşabilmesi için telefon numaranızı ve adresinizi restoranlarla paylaşıyoruz. Bunun dışında hiçbir sebeple bilgileriniz, restoranlarla ya da başka firmalarla paylaşılmamaktadır.                                                |
 
-  Scenario: User can list App about
-    Given I select city "TR_ISTANBUL"
-    When I can list about
-    Then I can validate about is valid
